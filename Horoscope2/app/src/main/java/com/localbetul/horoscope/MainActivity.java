a042166965e95542c3379e6fd0ab900b2941fad9
LOCAL BETUL <betulozturkmenoglu@gmail.com>
2016-12-08 23:06:28 +0300

    HCope
    Horoscope Project

--- Horoscope2/app/src/main/java/com/localbetul/horoscope/MainActivity.java
+++ Horoscope2/app/src/main/java/com/localbetul/horoscope/MainActivity.java
@@ -1,15 +1,13 @@
 package com.localbetul.horoscope;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Betül Öztürkmenoğlu 130201014 Proje Yaratıldı
     }
 }
